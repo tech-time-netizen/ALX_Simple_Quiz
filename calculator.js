7bfa48c608b3050a0825adb998535209dcefb1de
unknown <e2476861@gmail.com>
2025-06-15 13:07:22 +0300

    Build just a simple calculator

---
+++ calculator.js
@@ -0,0 +1,53 @@
+function add(number1, number2) {
+    return number1 + number2
+}
+
+function subtract(number1, number2) {
+    return number1 - number2
+}
+
+function multiply(number1, number2) {
+    return number1 * number2
+}
+
+function divide(number1, number2) {
+    
+    if (number2 === 0) {
+        return "Error. Division by zero"
+    }
+
+    return number1 / number2
+}
+
+const addBtn = document.getElementById("add");
+const subtractBtn = document.getElementById("subtract");
+const multiplyBtn = document.getElementById("multiply");
+const divdeBtn = document.getElementById("divide");
+
+addBtn.addEventListener('click', function () {
+    const number1 = parseFloat(document.getElementById("number1").value) || 0;
+    const number2 = parseFloat(document.getElementById("number2").value) || 0 ;
+    const result = add(number1, number2);
+    document.getElementById("result").innerText = `Result: ${result}`;
+})
+
+subtractBtn.addEventListener('click', function () {
+    const number1 = parseFloat(document.getElementById("number1").value) || 0;
+    const number2 = parseFloat(document.getElementById("number2").value) || 0;
+    const result = subtract(number1, number2);
+    document.getElementById("result").innerText = `Result: ${result}`;
+})
+
+multiplyBtn.addEventListener('click', function () {
+    const number1 = parseFloat(document.getElementById("number1").value) || 0;
+    const number2 = parseFloat(document.getElementById("number2").value) || 0;
+    const result = multiply(number1, number2);
+    document.getElementById("result").innerText = `Result: ${result}`;
+});
+
+divdeBtn.addEventListener('click', function () {
+    const number1 = parseFloat(document.getElementById("number1").value) || 0;
+    const number2 = parseFloat(document.getElementById("number2").value) || 0;
+    const result = divide(number1, number2);
+    document.getElementById("result").innerText = `Result: ${result}`;
+})
